b682b3fb4198f55d79208ab3e1f2ca915eef9fd4
Ahmet <ahmtguler@gmail.com>
2024-10-09 01:56:43 +0300

    swap function first draft finished

--- contracts/contracts/dex/router/Router.sol
+++ contracts/contracts/dex/router/Router.sol
@@ -6,17 +6,27 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {IPool} from "../pair/IPool.sol";
+import {IFeeManager} from "../../fee-manager/IFeeManager.sol";
 
 contract Router is ReentrancyGuard {
     using SafeERC20 for IERC20;
     using SafeERC20 for IPool;
 
-    IPool public pool;
-    IERC20 public TVER;
-    IERC20 public THB;
+    IPool public immutable pool;
+    IFeeManager public immutable feeManager;
+    IERC20 public immutable TVER;
+    IERC20 public immutable THB;
 
-    constructor(IPool _pool) {
+    constructor(
+        IPool _pool,
+        IFeeManager _feeManager,
+        IERC20 _TVER,
+        IERC20 _THB
+    ) {
         pool = _pool;
+        feeManager = _feeManager;
+        TVER = _TVER;
+        THB = _THB;
     }
 
     //todo add LP
@@ -76,8 +86,6 @@ contract Router is ReentrancyGuard {
         require(amountTHB >= amountTHBMin, "Router: insufficient THB amount");
     }
 
-    //todo swap 
-
     function swapExactTokens(
         uint256 amountIn,
         uint256 amountOutMin,
@@ -90,21 +98,18 @@ contract Router is ReentrancyGuard {
         require(amountIn > 0, "Router: insufficient amount");
         require(reserveTVER > 0 && reserveTHB > 0, "Router: insufficient liquidity");
 
-        uint256 amountOut;
-        if (direction == 0) {
-            amountOut = quote(amountIn, reserveTVER, reserveTHB);
+        if (direction == 0) { // TVER -> THB
+            uint256 amountOut = getAmountOut(amountIn, reserveTVER, reserveTHB);
             require(amountOut >= amountOutMin, "Router: insufficient output amount");
             TVER.safeTransferFrom(msg.sender, address(p), amountIn);
-            p.swap(amountOut, 0, recipient);
-        } else {
-            amountOut = quote(amountIn, reserveTHB, reserveTVER);
+            p.swap(0, amountOut, recipient);
+        } else { // THB -> TVER
+            uint256 amountOut = quote(amountIn, reserveTHB, reserveTVER);
             require(amountOut >= amountOutMin, "Router: insufficient output amount");
             THB.safeTransferFrom(msg.sender, address(p), amountIn);
-            p.swap(0, amountOut, recipient);
+            p.swap(amountOut, 0, recipient);
         }
     }
-
-    //todo quote
 
     function quote(uint256 amountA, uint256 reserveA, uint256 reserveB) private pure returns (uint256) {
         require(amountA > 0, 'Router: insufficient amount');
@@ -113,6 +118,12 @@ contract Router is ReentrancyGuard {
         return amountB;
     }
 
-
-    //todo getAmountOut
+    function getAmountOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut) private view returns (uint256) {
+        require(amountIn > 0, 'Router: insufficient amount');
+        require(reserveIn > 0 && reserveOut > 0, 'Router: insufficient liquidity');
+        uint256 amountInWithFee = amountIn * (10_000 - feeManager.getFee());
+        uint256 numerator = amountInWithFee * reserveOut;
+        uint256 denominator = (reserveIn * 10_000) + amountInWithFee;
+        return (numerator / denominator);
+    }
 }
