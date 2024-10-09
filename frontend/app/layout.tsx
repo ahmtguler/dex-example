2af11aee264f2367b9d342328c26d7f213efae86
Ahmet <ahmtguler@gmail.com>
2024-10-10 01:51:58 +0300

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 import {Providers} from '@/context'
 import Header from "@/components/header/header"
+import { Toaster } from "@/components/ui/sonner"
 
 export const metadata: Metadata = {
   title: "DEX Example",
@@ -28,6 +29,7 @@ export default function RootLayout({
           <Providers cookie={headers().get('cookie') ?? ''}>
             <Header />
             {children}
+            <Toaster richColors/>
           </Providers>
         </ThemeProvider>
       </body>
